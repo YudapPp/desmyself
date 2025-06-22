8985fc4c7c107c26276883327a7ada29b2d0043f
yuda <145818394+YudapPp@users.noreply.github.com>
2025-06-22 06:59:35 -0700

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,11 +1,9 @@
-const firebaseConfig = {
+var firebaseConfig = {
   apiKey: "AIzaSyASn0Qzfkf4SioQekrKh-zHlNyDA_ncDfg",
   authDomain: "krin-web.firebaseapp.com",
   projectId: "krin-web",
   storageBucket: "krin-web.firebasestorage.app",
   messagingSenderId: "930904994607",
   appId: "1:930904994607:web:54400f894aef18fb3aa7da",
-  measurementId: "G-XF8W52TRT1"
 };
-
 firebase.initializeApp(firebaseConfig);
